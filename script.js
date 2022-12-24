b9835b29b88ba9b5f8b22fcda09371615a7bd634
alvissk50 <64791852+alvissk50@users.noreply.github.com>
2022-12-24 22:06:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const nextXmasDate = (currentTime) => {
+  let xmasDate = new Date(currentTime.getFullYear() + "-12-24T22:10:00");
+  if (currentTime.getTime() > xmasDate.getTime()) {
+    let nextYear = currentTime.getFullYear() + 1;
+    xmasDate = new Date(nextYear + "-12-25T00:00:00");
+  }
+  return xmasDate;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Unix timestamp (in seconds) to count down to
+  let nextXmas = nextXmasDate(new Date()).getTime() / 1000;
+
+  // Set up FlipDown
+  let flipdown = new FlipDown(nextXmas)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+     
+      alert("C nhắn: T thì không định cưới nên chả care. vậy, đích đến cuối cùng của mqh này là gì? cho vui ư ... T cũng không nghĩ xa thế, tương lai không nói trước được nhưng ít nhất cũng đừng vùi dập nó chứ..");
+
+    });
+});
